6d6868f4caa28eb3ef084893f398374cf88c7824
root <root@feeny.(none)>
2013-12-09 03:14:56 +0000

    added reset script and manage wrapper

---
+++ reset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /home/djangoapps/spoilr
+MANAGE=../spoilr-env/bin/python\ manage.py
+
+# backup database, just in case
+mv /var/sqlitedb/hunt.db /var/sqlitedb/hunt.db.bak
+/usr/bin/mysqldump --events --all-databases > /home/hunt/spoilr.sql
+cat /home/hunt/mysqlinit.sql | /usr/bin/mysql -u root 
+
+
+# recreate database, this will ask for admin username/password
+$MANAGE syncdb --traceback
+
+# until I figure out who apache is running as, let everyone write the hunt db
+chmod a+w /var/sqlitedb/hunt.db
+
+# load hunt data
+$MANAGE load_data --traceback
+
+# publish team files
+$MANAGE republish --traceback
