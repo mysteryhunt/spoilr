a5410aa95c354c4e44a43141cb54893072b38732
James Clark <sbj@dimins.com>
2014-01-03 01:14:59 -0500

    Add django cache settings

--- spoilr/settings.py
+++ spoilr/settings.py
@@ -45,6 +45,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/cache/spoilr',
+    }
+}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
