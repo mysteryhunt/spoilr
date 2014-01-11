d517c08e1d1cdd5a13b67d2e59230c8d6a2235cc
root <root@corey.massaveunix.com>
2014-01-11 02:45:55 +0000

    update mysqlinit to do full permissions for replication and monitoring

--- mysqlinit.sql
+++ mysqlinit.sql
@@ -1,6 +1,13 @@
 -- totally wipe out spoilr database
 DROP DATABASE spoilr;
 CREATE DATABASE spoilr;
-GRANT ALL PRIVILEGES ON spoilr.* to spoilr @'localhost' IDENTIFIED BY 'spoilr';
+GRANT ALL PRIVILEGES ON spoilr.* to 'spoilr'@'%' IDENTIFIED BY 'spoilr';
+GRANT ALL PRIVILEGES ON spoilr.* to 'spoilr'@'localhost' IDENTIFIED BY 'spoilr';
+GRANT USAGE ON *.* to 'monitoring'@'%' IDENTIFIED BY 'dup3m3';
+GRANT USAGE ON *.* to 'monitoring'@'localhost' IDENTIFIED BY 'dup3m3';
+GRANT REPLICATION CLIENT on *.* to 'monitoring'@'%' identified by 'dup3m3';
+GRANT REPLICATION CLIENT on *.* to 'monitoring'@'localhost' identified by 'dup3m3';
+GRANT SELECT ON *.* to 'monitoring'@'%';
+GRANT SELECT ON *.* to 'monitoring'@'localhost';
 FLUSH PRIVILEGES;
 
