27fdac6701837bca29e7b546325ce378fc5ef530
root <root@corey.massaveunix.com>
2014-01-13 23:36:45 +0000

    add script for handling local and remote mysqlbackup (run this on a slave)

---
+++ mysqlbackup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TIMESTAMP=`date +%Y-%m-%d-%H:%M:%S`
+mkdir -p /root/mysqldumps/
+cd /root/mysqldumps
+/usr/bin/mysqldump -u root --all-databases --events > /root/mysqldumps/${TIMESTAMP}.sql && ls -1t /root/mysqldumps | tail -n 1 | xargs rm
+/usr/bin/rsync -avu --delete /root/mysqldumps/ root@topanga.massaveunix.com:/root/mysqldumps/ > /dev/null
+
