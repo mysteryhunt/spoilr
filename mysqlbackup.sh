0894c6c3d53caf3b80e0f72fdc593de5f9c26b38
root <root@corey.massaveunix.com>
2014-01-28 01:14:04 +0000

    remove custom and badly written mysqlbackup.sh script

--- mysqlbackup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TIMESTAMP=`date +%Y-%m-%d-%H:%M:%S`
-mkdir -p /root/mysqldumps/
-cd /root/mysqldumps
-/usr/bin/mysqldump -u root --add-drop-database --all-databases --events > /root/mysqldumps/${TIMESTAMP}.sql && ls -1t /root/mysqldumps | tail -n 1 | xargs rm
-/usr/bin/rsync -avu --delete /root/mysqldumps/ root@topanga.massaveunix.com:/root/mysqldumps/ > /dev/null
-
